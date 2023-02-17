fcd717842816f753095fbc853d396540e327cc20
elijah <cuiyeqing960904@gmail.com>
2023-02-17 11:34:54 -0500

    templates added

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,16 +4,23 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movie')]
-    public function index(): JsonResponse
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/MovieController.php',
         ]);
+    }
+
+    #[Route('/movies/{id}', name: 'show_movie', methods: ['GET'])]
+    public function show($id)
+    {
+
     }
 }
