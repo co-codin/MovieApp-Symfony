588dec514e905a4e01158f751116b7eb03c97a71
elijah <cuiyeqing960904@gmail.com>
2023-02-20 01:44:54 -0500

    create movie

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -7,6 +7,7 @@ use App\Form\MovieFormType;
 use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -50,6 +51,37 @@ class MovieController extends AbstractController
         $movie = new Movie();
         $form = $this->createForm(MovieFormType::class, $movie);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newMovie = $form->getData();
+            $imagePath = $form->get('imagePath')->getData();
+
+            if ($imagePath) {
+                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+
+                try {
+                    $imagePath->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileException $e) {
+                    return new Response($e->getMessage());
+                }
+
+                $newMovie->setUserId($this->getUser()->getId());
+                $newMovie->setImagePath('/uploads/' . $newFileName);
+            }
+
+            $this->em->persist($newMovie);
+            $this->em->flush();
+
+            return $this->redirectToRoute('movies');
+        }
+
+        return $this->render('movies/create.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     #[Route('/movies/{id}', name: 'delete_movie', methods: ['DELETE'])]
